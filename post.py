6caa08931a364295b6ffd0bc5dcc8a86e2a3241a
rekimuhammadakbar@gmail.com <107263378+Rekimuhamad@users.noreply.github.com>
2022-09-25 19:08:43 +0700

    Create post.py

---
+++ post.py
@@ -0,0 +1,44 @@
+from sqlite3 import Timestamp
+from flask import Flask,request
+import pymongo
+import datetime
+
+app = Flask(__name__)
+
+client = pymongo.MongoClient("mongodb+srv://Rext:<rekiakbar>@cluster0.ha5j7v8.mongodb.net/?retryWrites=true&w=majority")
+
+db = client.reki
+collection = db.smkn4
+
+@app.route('/week8',methods=['GET','POST'])
+def location_application():
+    kecepatan= request.args.get('kecepatan')
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+    save_to_db(kecepatan=kecepatan,latitude=latitude,longitude=longitude)
+    return{
+        "output":{
+            "kecepatan":kecepatan,
+            "latitude":latitude,
+            "longitude":longitude,
+            "timestamp": datetime.datetime.now()
+        }
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+def assignment(kecepatan,latitude,longitude) -> tuple:
+    try:
+        data = {
+            "kecepatan": kecepatan,
+            "latitude": latitude,
+            "logitude": longitude,
+            "Timestamp": datetime.datetime.now()
+        }
+
+        results = collection.insert_one(data)
+        print(results.inserted_ids)
+        return True,None
+    except Exception as e:
+      return False,str(e)
